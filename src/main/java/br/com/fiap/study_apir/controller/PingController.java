54e696201d03b239baad8edbb2edace6eddc64d7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 22:18:35 -0300

    feat: adiciona package controller e modifica pom

--- src/main/java/br/com/fiap/study_apir/controller/PingController.java
+++ src/main/java/br/com/fiap/study_apir/controller/PingController.java
@@ -1,4 +1,4 @@
-package br.com.fiap.study_apir;
+package br.com.fiap.study_apir.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PingController {
 
     @GetMapping("/ping")
-    public String ping(){
+    public String ping() {
         return "pong";
     }
 
-
+    @GetMapping("/rota1")
+    public String rota1() {
+        return "rota";
+    }
 }
